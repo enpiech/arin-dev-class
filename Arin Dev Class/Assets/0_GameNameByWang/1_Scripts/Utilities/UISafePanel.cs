6a8f3ffc4761467c513a9e9667600b4266aca859
Quang cún <94778939+superidolbetterthanlaudaitinhai@users.noreply.github.com>
2023-02-10 16:42:06 +0700

    Make tank and enemy move
    scene riêng vs folder riêng cho e r nha a

---
+++ Arin Dev Class/Assets/0_GameNameByWang/1_Scripts/Utilities/UISafePanel.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+[RequireComponent(typeof(RectTransform))]
+public class UISafePanel : MonoBehaviour
+{
+    private RectTransform rectTransform;
+    private Rect lastSafeArea;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void OnEnable()
+    {
+        if (lastSafeArea != Screen.safeArea)
+        {
+            lastSafeArea = Screen.safeArea;
+            Refresh();
+        }
+    }
+
+    public void Refresh()
+    {
+        var anchorMin = lastSafeArea.position;
+        var anchorMax = lastSafeArea.position + lastSafeArea.size;
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+        rectTransform.anchorMin = anchorMin;
+        rectTransform.anchorMax = anchorMax;
+    }
+}
